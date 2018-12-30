3f977775e841a5f68143efafbe3cc26c97611e7b
mhmodsayid <31825867+mhmodsayid@users.noreply.github.com>
2018-12-30 21:53:21 +0200

    Update ClientConsole.java

--- ClientPrototype/src/controller/ClientConsole.java
+++ ClientPrototype/src/controller/ClientConsole.java
@@ -9,7 +9,6 @@ public class ClientConsole extends Application {
 	public static void main(String[] args) throws Exception {
 	String host = "";
 		//test
-		//test3
 		try {
 			host = args[0];
 		} catch (ArrayIndexOutOfBoundsException e) {
