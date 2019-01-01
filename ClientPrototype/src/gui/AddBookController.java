e9a7046fe6a1fed057bb425c5bacb7304ab0253f
asus <asus@10.10.1.50>
2019-01-01 15:11:15 +0200

    011

--- ClientPrototype/src/gui/AddBookController.java
+++ ClientPrototype/src/gui/AddBookController.java
@@ -3,6 +3,9 @@ package gui;
 public class AddBookController {
 //mhmod
 //fares
+public void hello() {
+	
+}
 private void sysout() {
 	// TODO Auto-generated method stub
 System.out.println("fefo");
