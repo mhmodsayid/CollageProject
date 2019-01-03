7cdf58a36b304f7045167ed6c4739bf0d11187b6
mahmouds <mahmouds@MTL-MAHMOUDS.mtl.com>
2019-01-03 22:47:56 +0200

    remove some imports

--- clientprototype/src/gui/MainPage.java
+++ clientprototype/src/gui/MainPage.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
